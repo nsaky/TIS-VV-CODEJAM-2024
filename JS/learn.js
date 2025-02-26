619df656ebf66c9c1f0ff4ddc7ddd884e01ec084
Yasir Eqbal <140412597+nsaky@users.noreply.github.com>
2025-02-26 12:16:14 +0530

    Add files via upload

---
+++ JS/learn.js
@@ -0,0 +1,80 @@
+document.querySelector(".quiz-1-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-1/quiz-1.html", "_self");
+}
+
+document.querySelector(".quiz-2-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-2/quiz-2.html", "_self");
+}
+
+document.querySelector(".quiz-3-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-3/quiz-3.html", "_self");
+}
+
+document.querySelector(".quiz-4-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-4/quiz-4.html", "_self");
+}
+
+
+document.querySelector(".quiz-5-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-5/quiz-5.html", "_self");
+}
+
+document.querySelector(".quiz-6-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-6/quiz-6.html", "_self");
+}
+
+document.querySelector(".quiz-7-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-7/quiz-7.html", "_self");
+}
+
+document.querySelector(".quiz-8-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-8/quiz-8.html", "_self");
+}
+
+document.querySelector(".quiz-9-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-9/quiz-9.html", "_self");
+}
+
+document.querySelector(".quiz-10-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-10/quiz-10.html", "_self");
+}
+
+document.querySelector(".quiz-11-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-11/quiz-11.html", "_self");
+}
+
+document.querySelector(".quiz-12-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-12/quiz-12.html", "_self");
+}
+
+document.querySelector(".quiz-13-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-13/quiz-13.html", "_self");
+}
+
+document.querySelector(".quiz-14-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-14/quiz-14.html", "_self");
+}
+
+document.querySelector(".quiz-15-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-15/quiz-15.html", "_self");
+}
+
+document.querySelector(".quiz-16-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-16/quiz-16.html", "_self");
+}
+
+document.querySelector(".quiz-17-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-17/quiz-17.html", "_self");
+}
+
+document.querySelector(".quiz-18-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-18/quiz-18.html", "_self");
+}
+
+document.querySelector(".quiz-19-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-19/quiz-19.html", "_self");
+}
+
+document.querySelector(".quiz-20-btn").onclick = ()=>{
+    window.open("../Quiz/Quiz-20/quiz-20.html", "_self");
+}
